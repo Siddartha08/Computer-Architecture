921174f70e8c6c9febcbec76bc239cb449c0a31e
unknown <jemery89@gmail.com>
2019-09-04 23:00:38 -0500

    jp-emery: day 3 work complete

--- simply.py
+++ simply.py
@@ -13,6 +13,7 @@ RET          = 11
 ram = [0] * 256
 registers = [0] * 8 
 ​
+SP = 0xF3
 PC = 0
 running = True
 ​
@@ -36,80 +37,132 @@ def load_memory(filename):
         print('I cannot find that file, check the name')
         sys.exit(2)
 ​
-program = [
-    PRINT_TIM,
-    PRINT_NUM,
-    99,
-    SAVE,
-    2,
-    99,
-]
+# program = [
+#     PRINT_TIM,
+#     PRINT_NUM,
+#     99,
+#     SAVE,
+#     2,
+#     99,
+# ]
 ​
 load_memory(sys.argv[1])
 ​
-# def ram_read()
+def alu(self, command, a, b):
+    if command == "ADD":
+        registers[a] += registers[b]
+    elif command == "MUL":
+        mul = registers[a] * registers[b]
+        registers[a] = mul
+    else:
+        raise Exception("operation not supported")
 
-# def ram_write()
+
+
+def ram_read(self, MAR):
+    return ram[MAR]
+
+def ram_write(self, MAR, MDR):
+    ram[MAR] = MDR
 
 while running:
-    command = ram[PC]
-​
-    if command == PRINT_TIM:
-        print('Tim!')
-        PC += 1
-​
-    elif command == PRINT_NUM:
-        num = ram[PC + 1]
-        print(num)
-        PC += 2
-​
-    elif command == PRINT_SUM:
-        first_number = ram[PC + 1]
-        second_number = ram[PC + 2]
-        print(first_number + second_number)
-        PC += 3
-​
-    elif command == SAVE:
-        register = ram[PC + 1]
-        number_to_save = ram[PC + 2]
-        registers[register] = number_to_save
-        PC += 3
-​
-    elif command == ADD:
-        first_register = ram[PC + 1]
-        second_register = ram[PC + 2]
-        sum = registers[first_register] + registers[second_register]
-        registers[first_register] = sum
-        PC += 3
-​
-    elif command == PUSH:
-        registers[7] = ( registers[7] -1) % 255
-        SP = registers[7]
-        # registers[7] = ( SP - 1 ) % 255
-​
-        register_address = ram[PC + 1]
-        value = registers[register_address]
-​
-        ram[SP] = value
-        PC += 2
-​
-    elif command == POP:
-        SP = registers[7]
-​
-        value = ram[SP]
-        register_address = ram[PC + 1]
-        registers[register_address] = value
-​
-       
-        registers[7] = ( SP + 1 ) % 255
-​
-        PC += 2
 
-    elif command == PRINT_REG:
-        register_address = ram[pc + 1]
-        value_to_print = registers[register_address]
-        print(value_to_print)
-        pc += 2
+    LDI = 0b10000010
+    PRN = 0b01000111
+    HLT = 0b00000001
+    MUL = 0b10100010
+    # day 3
+    PUSH = 0b01000101
+    POP = 0b01000110
+
+    command = ram[PC]
+​    
+    a = self.ram_read(PC + 1)
+    b = self.ram_read(PC + 2)
+
+    if command == LDI:
+        registers[a] = b
+        PC += 3
+    elif command == PRN:
+        print(registers[a])
+        PC += 2
+    elif command == MUL:
+        self.alu("MUL", a,b)
+        PC += 3
+    elif command == PUSH:
+        SP = (SP % 255) - 1
+        ram[SP] = registers[a]
+        PC += 2
+    elif command == POP:
+        registers[a] = ram[SP]
+        SP = (SP % 255) + 1
+        PC += 2
+    elif command == CALL
+
+    elif command == HLT:
+        running = False
+    else:
+        print('Command not recognized')
+        running = False
+    
+
+
+#     if command == PRINT_TIM:
+#         print('Tim!')
+#         PC += 1
+# ​
+#     elif command == PRINT_NUM:
+#         num = ram[PC + 1]
+#         print(num)
+#         PC += 2
+# ​
+#     elif command == PRINT_SUM:
+#         first_number = ram[PC + 1]
+#         second_number = ram[PC + 2]
+#         print(first_number + second_number)
+#         PC += 3
+# ​
+#     elif command == SAVE:
+#         register = ram[PC + 1]
+#         number_to_save = ram[PC + 2]
+#         registers[register] = number_to_save
+#         PC += 3
+# ​
+#     elif command == ADD:
+#         first_register = ram[PC + 1]
+#         second_register = ram[PC + 2]
+#         sum = registers[first_register] + registers[second_register]
+#         registers[first_register] = sum
+#         PC += 3
+# ​
+#     elif command == PUSH:
+#         registers[7] = ( registers[7] -1) % 255
+#         SP = registers[7]
+#         # registers[7] = ( SP - 1 ) % 255
+# ​
+#         register_address = ram[PC + 1]
+#         value = registers[register_address]
+# ​
+#         ram[SP] = value
+#         PC += 2
+# ​
+#     elif command == POP:
+#         SP = registers[7]
+# ​
+#         value = ram[SP]
+#         register_address = ram[PC + 1]
+#         registers[register_address] = value
+# ​
+       
+#         registers[7] = ( SP + 1 ) % 255
+# ​
+#         PC += 2
+
+#     elif command == PRINT_REG:
+#         register_address = ram[pc + 1]
+#         value_to_print = registers[register_address]
+#         print(value_to_print)
+#         pc += 2
 ​
     elif command == CALL:
         register_address = ram[PC + 1]
@@ -131,6 +184,8 @@ while running:
         SP = registers[7]
         address_to_return_to = ram[SP]
 
+        registers[7] = (SP + 1) % 255
+
         PC = address_to_return_to
 
     elif command == HALT:
@@ -143,3 +198,7 @@ while running:
 
 # what was the next thing that was to be done after call
 # remember the next step after call was run
+
+
+# jump 
+# conditional jumps
